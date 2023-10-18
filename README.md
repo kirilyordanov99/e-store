8d474b3f06650753e30e8c97669011c80cfd2254
kirilyordanov99 <32341596+kirilyordanov99@users.noreply.github.com>
2023-10-18 11:29:37 +0300

    front end

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # e-store
 fake store with react(fronted part only, at least for now)
 I might add some backend with php in the future...
+to start the app chande folder in command prompt
+install npm install
+and start with npm start :) 
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Footer, Navbar } from "../components";
+const AboutPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">About Us</h1>
+        <hr />
+        <p className="lead text-center">
+        Fake store, this isn't real store, this won't be commercial site,
+        this is something just for fun, and for me to lear bit of react...
+        I just want to try and use theme, with different pages and navigation.
+        Fake store, this isn't real store, this won't be commercial site,
+        this is something just for fun, and for me to lear bit of react...
+        I just want to try and use theme, with different pages and navigation
+        Fake store, this isn't real store, this won't be commercial site,
+        this is something just for fun, and for me to lear bit of react...
+        I just want to try and use theme, with different pages and navigation
+        Fake store, this isn't real store, this won't be commercial site,
+        this is something just for fun, and for me to lear bit of react...
+        I just want to try and use theme, with different pages and navigation.
+        </p>
+
+        <h2 className="text-center py-4">Our Products</h2>
+        <div className="row">
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Mens's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/7679720/pexels-photo-7679720.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Women's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/1927259/pexels-photo-1927259.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Jewelery</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Electronics</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default AboutPage
